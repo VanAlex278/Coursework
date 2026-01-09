f383786d9c7c37174cd114eaf4c2ecc9776facf0
Van Alex <van_alexandr@rambler.ru>
2026-01-09 20:42:11 +0400

    create test_reports.py and test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,16 @@
-from src.utils import get_greeting
+import pandas as pd
+from unittest.mock import patch, mock_open
+from src.utils import load_transactions, get_greeting, json_file_reader
+
+
+@patch("pandas.read_excel")
+def test_load_transactions(mock_excel):
+    expected = pd.DataFrame({"Дата операции": ["15.01.2020 16:44:00", "14.01.2020 16:42:04"], "Номер карты": ["*7197", "*5814"]})  # pd.DataFrame()
+    mock_excel.return_value = expected
+    result = load_transactions("test_file")
+    result["Дата операции"] = result["Дата операции"].astype(str)
+    assert result["Дата операции"][0] == '2020-01-15 16:44:00'
+    assert result["Номер карты"][0] == "*7197"
 
 
 def test_get_greeting_morning():
@@ -9,3 +21,12 @@ def test_get_greeting_morning():
 def test_get_greeting_day():
     """Тест приветствия для дня."""
     assert get_greeting("2020-01-01 14:00:00") == "Добрый день"
+
+
+@patch("json.load")
+def test_json_file_reader(mock_load):
+    with patch("builtins.open", mock_open()) as mocked_open:
+        mock_load.return_value = [{'test_1': 'test_data'}]
+        result = json_file_reader("dummy data")
+        expected = [{'test_1': 'test_data'}]
+        assert result == expected
